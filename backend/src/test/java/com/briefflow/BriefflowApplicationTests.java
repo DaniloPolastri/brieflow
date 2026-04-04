ab83e2d138a088481a0fb5b6a48a78c4157f9f52
Danilo <danilopolastri.ti@gmail.com>
2026-04-04 11:59:37 -0300

    chore: scaffold Spring Boot backend with Maven

---
+++ backend/src/test/java/com/briefflow/BriefflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briefflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriefflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
